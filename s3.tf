3e68003afe5824caed5c5ab9c56299f553a297ec
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-07-01 20:16:24 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -22,10 +22,3 @@ versioning_configuration {
 status = "Enabled"
 }
 }
-terraform {
-backend "s3" {
-region = "ap-south-1"
-bucket = "sairam.devops.project.bucket"
-key = "prod/terraform.tfstate"
-}
-}
