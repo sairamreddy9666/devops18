5ddbcbc046007b20e901b8a88c6f0bf602918a3e
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-07-11 14:28:51 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,3 @@
-terraform {
-  required_version = ">= 1.3.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 provider "aws" {
-  region  = "ap-south-1"
-  profile = "default"  # Optional, if you're using AWS CLI named profiles
+  region = "ap-south-1"
 }
